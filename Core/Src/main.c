888a91a1033ede8205b32e1f8c05c1e4d251c652
Diveturtle93 <hafemeister1@hotmail.de>
2026-06-10 07:08:46 +0200

    Upgrade auf neuste Version
    - Firmware Package 1.17.4
    - MX-Cube 6.16.1 ; 6.0.161

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1050,8 +1050,7 @@ void Error_Handler(void)
 	while (1);
   /* USER CODE END Error_Handler_Debug */
 }
-
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
